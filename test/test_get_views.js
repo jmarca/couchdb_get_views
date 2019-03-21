c68a5aee118214bc33b389fcbd5f7af0f2975f7e
James E. Marca <james@activimetrics.com>
2019-03-21 12:39:07 -0700

    test_get_views updated, passing

--- test/test_get_views.js
+++ test/test_get_views.js
@@ -95,112 +95,138 @@ config_okay(config_file)
                                })
 
                        })
-    })
 
-    // it('should get all missing wim neighbors in district 3, no reduce, using keys'
-    //   ,function(done){
-    //        viewer(_.assign({}
-    //                       ,config.couchdb
-    //                       ,{'doc':'doc1'
-    //                        ,'view':views[0]
-    //                        ,'keys':[[2007, 3,5],[2008,3,5]]
-    //                        ,'reduce':false
-    //                        })
-    //              ,function(err,docs){
-    //                   should.not.exist(err)
-    //                   docs.rows.should.have.property('length',82)
-    //                   _.each(docs.rows,function(doc){
-    //                       doc.key[0].should.match(/200(7|8)/)
-    //                       doc.key[1].should.eql(3)
-    //                       doc.key[2].should.eql(5)
-    //                   });
-    //                   return done()
-    //               })
-    //    })
-    // it('should get 10 missing wim neighbors in district 3, no reduce, using keys'
-    //   ,function(done){
-    //        viewer(_.assign({}
-    //                       ,config.couchdb
-    //                       ,{'doc':'doc1'
-    //                        ,'view':views[0]
-    //                        ,'keys':[[2007, 3,5],[2008,3,5]]
-    //                        ,'limit':10
-    //                        ,'reduce':false
-    //                        })
-    //              ,function(err,docs){
-    //                   should.not.exist(err)
-    //                   docs.rows.should.have.property('length',10)
-    //                   _.each(docs.rows,function(doc){
-    //                       doc.key[0].should.match(/200(7|8)/)
-    //                       doc.key[1].should.eql(3)
-    //                       doc.key[2].should.eql(5)
-    //                   });
-    //                   return done()
-    //               })
-    //    })
-    // it('should get 1 missing wim neighbors in district 3, no reduce, using keys, descending'
-    //   ,function(done){
-    //        viewer(_.assign({}
-    //                       ,config.couchdb
-    //                       ,{'doc':'doc1'
-    //                        ,'view':views[0]
-    //                        ,'startkey':[2007,3,{}] // year, district, freeway
-    //                        ,'limit':1
-    //                        ,'descending':true
-    //                        ,'reduce':false
-    //                        })
-    //              ,function(err,docs){
-    //                   should.not.exist(err)
-    //                   docs.rows.should.have.property('length',1)
-    //                   _.each(docs.rows,function(doc){
-    //                       doc.key[0].should.match(/2007/)
-    //                       doc.key[1].should.eql(3)
-    //                       doc.key[2].should.eql(275) // the last freeway in my testdb
-    //                   });
-    //                   return done()
-    //               })
-    //    })
-    // it('should get 1 missing wim neighbors in district 3, no reduce, using keys, descending false'
-    //   ,function(done){
-    //        viewer(_.assign({}
-    //                       ,config.couchdb
-    //                       ,{'doc':'doc1'
-    //                        ,'view':views[0]
-    //                        ,'startkey':[2007,3,{}] // year, district, freeway
-    //                        ,'limit':1
-    //                        ,'descending':false
-    //                        ,'reduce':false
-    //                        })
-    //              ,function(err,docs){
-    //                   should.not.exist(err)
-    //                   docs.rows.should.have.property('length',1)
-    //                   _.each(docs.rows,function(doc){
-    //                       doc.key[0].should.match(/2007/)
-    //                       doc.key[1].should.eql(4) // next one after 3 in my testdb
-    //                       doc.key[2].should.eql(1) // the first freeway in d4 in my testdb
-    //                   });
-    //                   return done()
-    //               })
-    //    })
-    // it('should get docs with include doc'
-    //   ,function(done){
-    //        viewer(_.assign({}
-    //                       ,config.couchdb
-    //                       ,{'view':views[0]
-    //                        ,'key':[2007,3,5]
-    //                        ,'reduce':false
-    //                        ,'include_docs':true
-    //                        })
-    //              ,function(err,docs){
-    //                   should.not.exist(err)
-    //                   docs.rows.should.have.property('length',42)
-    //                   _.each(docs.rows,function(doc){
-    //                       doc.key.should.eql([2007,3,5])
-    //                       var docdoc = doc.doc
-    //                       docdoc.should.have.property('2007')
-    //                       docdoc[2007].should.have.property('properties')
-    //                       docdoc[2007]['properties'][0].should.have.property('geojson')
-    //                   });
-    //                   return done()
-    //               })
-    //    })
+        await tap.test('should get all missing wim neighbors in district 3, no reduce, using keys'
+                       , t =>{
+                           utils.promise_wrapper(viewer
+                                                 ,_.assign({}
+                                                           ,config.couchdb
+                                                           ,{'doc':'doc1'
+                                                             ,'view':views[0]
+                                                             ,'keys':[[2007, 10,5],[2008,10,5]]
+                                                             ,'reduce':false
+                                                            }))
+                               .then( docs =>{
+                                   t.ok(docs.rows)
+                                   t.equals(docs.rows.length,33)
+                                   _.each(docs.rows,function(doc){
+                                       t.match(doc.key[0],/200(7|8)/)
+                                       t.equal(doc.key[1],10)
+                                       t.equal(doc.key[2],5)
+                                   })
+                                   return t.end()
+                               })
+                               .catch( e => {
+                                   console.log('error',e)
+                                   t.fail()
+                               })
+                       })
+
+        await tap.test('should get 10 missing wim neighbors in district 3, no reduce, using keys'
+                       , t =>{
+                           utils.promise_wrapper(viewer
+                                                 ,_.assign({}
+                                                           ,config.couchdb
+                                                           ,{'doc':'doc1'
+                                                             ,'view':views[0]
+                                                             ,'keys':[[2007, 10,5],[2008,10,5]]
+                                                             ,'limit':10
+                                                             ,'reduce':false
+                                                            }))
+                               .then( docs =>{
+                                   t.ok(docs.rows)
+                                   t.equals(docs.rows.length,10)
+                                   _.each(docs.rows,function(doc){
+                                       t.match(doc.key[0],/200(7|8)/)
+                                       t.equal(doc.key[1],10)
+                                       t.equal(doc.key[2],5)
+                                   })
+                                   return t.end()
+                               })
+                               .catch( e => {
+                                   console.log('error',e)
+                                   t.fail()
+                               })
+                       })
+        await tap.test('should get 1 missing wim neighbors in district 3, no reduce, using keys, descending'
+                       , t =>{
+                           utils.promise_wrapper(viewer
+                                                 ,_.assign({}
+                                                           ,config.couchdb
+                                                           ,{'doc':'doc1'
+                                                             ,'view':views[0]
+                                                             ,'startkey':[2007,10,{}] // year, district, freeway
+                                                             ,'limit':1
+                                                             ,'descending':true
+                                                             ,'reduce':false
+                                                            }))
+                               .then( docs =>{
+                                   t.ok(docs.rows)
+                                   t.equals(docs.rows.length,1)
+                                   _.each(docs.rows,function(doc){
+                                       t.match(doc.key[0],/2007/)
+                                       t.equal(doc.key[1],10)
+                                       t.equal(doc.key[2],152) // the last freeway in my testdb
+                                   });
+                                   return t.end()
+                               })
+                               .catch( e => {
+                                   console.log('error',e)
+                                   t.fail()
+                               })
+                       })
+        await tap.test('should get 1 missing wim neighbors in district 3, no reduce, using keys, descending false'
+                       , t =>{
+                           utils.promise_wrapper(viewer
+                                                 ,_.assign({}
+                                                           ,config.couchdb
+                                                           ,{'doc':'doc1'
+                                                             ,'view':views[0]
+                                                             ,'startkey':[2007,10,{}] // year, district, freeway
+                                                             ,'limit':1
+                                                             ,'descending':false
+                                                             ,'reduce':false
+                                                            }))
+                               .then( docs =>{
+                                   t.ok(docs.rows)
+                                   t.equals(docs.rows.length,1)
+                                   _.each(docs.rows,function(doc){
+                                       t.match(doc.key[0],/2007|8/)
+                                       t.equal(doc.key[1],10)
+                                       t.equal(doc.key[2],4)
+                                   });
+                                   return t.end()
+                               })
+                               .catch( e => {
+                                   console.log('error',e)
+                                   t.fail()
+                               })
+                       })
+        await tap.test('should get docs with include doc'
+                       , t =>{
+                           utils.promise_wrapper(viewer
+                                                 ,_.assign({}
+                                                           ,config.couchdb
+                                                           ,{'view':views[0]
+                                                             ,'key':[2007,10,5]
+                                                             ,'reduce':false
+                                                             ,'include_docs':true
+                                                            }))
+                               .then( docs =>{
+                                   t.ok(docs.rows)
+                                   t.equals(docs.rows.length,17)
+                                   _.each(docs.rows,function(doc){
+                                       t.same(doc.key,[2007,10,5])
+                                       const docdoc = doc.doc
+                                       t.ok(docdoc['2007'])
+                                       t.ok(docdoc[2007]['properties'])
+                                       t.ok(docdoc[2007]['properties'][0]['geojson'])
+                                   });
+                                   return t.end()
+                               })
+                               .catch( e => {
+                                   console.log('error',e)
+                                   t.fail()
+                               })
+                       })
+    })
